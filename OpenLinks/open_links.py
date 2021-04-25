76cf6bf75007cf3a6f76ae71ebb02e6e748d6d7f
Yue Wu <yuw264@ucsd.edu>
2021-04-24 21:00:20 -0700

    Updated open_links

--- OpenLinks/open_links.py
+++ OpenLinks/open_links.py
@@ -3,14 +3,17 @@ import webbrowser
 import datetime
 
 if __name__ == '__main__':
-  with open('link_config.json', 'r+', encoding = 'utf-8') as f:
-    data = json.load(f)
-    for resolution in data['resolutions']:
-      res_nums = resolution.split('x')
-      url = 'https://www.pixiv.net/search.php?word={0}&order={1}&mode={2}&wlt={3}&wgt={3}&hlt={4}&hgt={4}&scd={5}'.format(data['keyword'], data['order'], data['mode'], res_nums[0], res_nums[1], data['last_update'])
-      webbrowser.open(url)
-    
-    data['last_update'] = str(datetime.date.today())
-    f.seek(0) # reset file position to beginning
-    json.dump(data, f, ensure_ascii = False, indent = 2)
-    f.truncate() # remove remaining part
+    with open('link_config.json', 'r+', encoding='utf-8') as f:
+        data = json.load(f)
+        for resolution in data['resolutions']:
+            res_nums = resolution.split('x')
+            width = int(res_nums[0])
+            height = int(res_nums[1])
+            o = data['offset']
+            url = f'https://www.pixiv.net/search.php?word={data["keyword"]}&order={data["order"]}&mode={data["mode"]}&wlt={width - o}&wgt={width + o}&hlt={height - o}&hgt={height + o}&scd={data["last_update"]}'
+            webbrowser.open(url)
+
+        data['last_update'] = str(datetime.date.today())
+        f.seek(0)  # reset file position to beginning
+        json.dump(data, f, ensure_ascii=False, indent=2)
+        f.truncate()  # remove remaining part
